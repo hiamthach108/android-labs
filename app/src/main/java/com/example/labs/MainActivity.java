c57ee5f82e665751bb3456253280dd793870d310
Ngoc Thach <dreon1008@gmail.com>
2025-02-22 04:44:58 +0700

    combine activities

--- app/src/main/java/com/example/labs/MainActivity.java
+++ app/src/main/java/com/example/labs/MainActivity.java
@@ -1,24 +1,90 @@
 package com.example.labs;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button lab1q1a = findViewById(R.id.lab1q1a);
+        Button lab1q1b = findViewById(R.id.lab1q1b);
+        Button lab1q2 = findViewById(R.id.lab1q2);
+        Button lab1q3 = findViewById(R.id.lab1q3);
+        Button lab2q1 = findViewById(R.id.lab2q1);
+        Button lab2q2 = findViewById(R.id.lab2q2);
+        Button lab2q3 = findViewById(R.id.lab2q3);
+        Button lab3q1 = findViewById(R.id.lab3q1);
+        Button lab3q2 = findViewById(R.id.lab3q2);
+
+        lab1q1a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivityLab1q1a.class));
+            }
+        });
+
+        lab1q1b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivityLab1q1b.class));
+            }
+        });
+
+        lab1q2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivityLab1q2.class));
+            }
+        });
+
+        lab1q3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivityLab1q3.class));
+            }
+        });
+
+        lab2q1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivityLab2q1.class));
+            }
+        });
+
+        lab2q2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivityLab2q2.class));
+            }
+        });
+
+        lab2q3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivityLab2q3.class));
+            }
+        });
+
+        lab3q1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivityLab3q1.class));
+            }
+        });
+
+        lab3q2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MainActivityLab3q2.class));
+            }
         });
     }
 }
---
+++ app/src/main/java/com/example/labs/MainActivityLab1q1a.java
@@ -0,0 +1,18 @@
+package com.example.labs;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivityLab1q1a extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lab1_1a);
+    }
+}
